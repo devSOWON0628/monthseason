e315d600fd164b180edf9c947358133ea4c1adce
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-06 19:52:38 +0900

    월을 입력받아 계절 출력

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace monthseason
 {
@@ -10,6 +12,44 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("월 입력 : ");
+            string month =Console.ReadLine();
+            string num =month.Replace("월", "");
+            int res = int.Parse(num);
+
+            switch (res)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을");
+                    break;
+
+                default:
+                    Console.WriteLine("월을 잘못 입력하셨습니다.");
+                    break;
+            }
+
+
+
         }
     }
 }
